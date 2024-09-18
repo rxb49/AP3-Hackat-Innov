f4273beb7d7ad3f723dfb4f53684501c19a34ee5
Robin <robintijou05@gmail.com>
2024-09-18 20:57:22 +0200

    ajout du about

--- resources/views/main/about.blade.php
+++ resources/views/main/about.blade.php
@@ -3,7 +3,56 @@
 @section('title', ' - About')
 
 @section('content')
-    <div class="d-flex justify-content-center full-height align-items-center">
-        <h1>ABOUT</h1>
+    <div class="d-flex justify-content-center full-height align-items-center px-5">
+        <p>À propos de notre site<br>
+
+            Bienvenue sur Hackat'innov !<br>
+
+            Nous sommes ravis de vous accueillir sur notre plateforme. Durant généralement un week­end, des développeurs volontaires se réunissent pour faire de la
+            programmation informatique collaborative en fonction d'un thème précis. . Nous mettons tout en œuvre pour vous offrir une expérience utilisateur agréable et sécurisée.
+            <br>
+            Protection des données personnelles
+            <br>
+            Conformément au Règlement Général sur la Protection des Données (RGPD), nous nous engageons à protéger vos données personnelles. Voici comment nous procédons :
+            <br>
+            Collecte des données : Nous recueillons uniquement les données nécessaires à améliorer notre service et vous envoyer des newsletters. Les types de données collectées incluent nom, adresse e-mail.
+            <br>
+            Utilisation des données : Les informations que nous recueillons sont utilisées exclusivement pour personnaliser votre expérience et répondre à vos demandes. Nous ne partageons pas vos données avec des tiers sans votre consentement, sauf si cela est requis par la loi.
+            <br>
+            Conservation des données : Vos données sont conservées pendant 1 an.
+            <br>
+            Sécurité des données : Nous mettons en place des mesures techniques et organisationnelles appropriées pour protéger vos données contre tout accès non autorisé, modification, divulgation ou destruction.
+            <br>
+            Cookies
+            <br>
+            Notre site utilise des cookies pour améliorer la navigation, analyser l'utilisation du site. Les cookies sont de petits fichiers texte stockés sur votre appareil lorsque vous visitez notre site. Vous pouvez configurer votre navigateur pour refuser les cookies ou pour vous avertir lorsqu'un cookie est envoyé.
+            <br>
+            Cookies essentiels : Ces cookies sont nécessaires au fonctionnement de notre site et ne peuvent pas être désactivés.
+            <br>
+            Cookies de performance : Ils nous permettent d'analyser l'utilisation de notre site et d'améliorer nos services.
+            <br>
+            Cookies de ciblage/publicité : Nous utilisons ces cookies pour vous montrer des publicités pertinentes en fonction de vos intérêts.
+            <br>
+            Vous pouvez gérer vos préférences en matière de cookies via les paramètres de votre navigateur. Pour plus d'informations sur les cookies et la façon de les gérer, veuillez consulter lien vers une politique de cookies détaillée à ajouter.
+            <br>
+            Vos droits
+            <br>
+            En vertu du RGPD, vous disposez de plusieurs droits concernant vos données personnelles, notamment :
+            <br>
+            Le droit d'accès : Vous pouvez demander une copie des données que nous détenons sur vous.<br>
+            Le droit de rectification : Vous pouvez demander la correction de données incorrectes ou incomplètes.<br>
+            Le droit à l'effacement : Vous pouvez demander la suppression de vos données sous certaines conditions.<br>
+            Le droit à la limitation du traitement : Vous pouvez demander de limiter le traitement de vos données.<br>
+            Le droit d'opposition : Vous pouvez vous opposer au traitement de vos données dans certains cas.<br>
+            Le droit à la portabilité : Vous pouvez demander de recevoir vos données dans un format structuré et couramment utilisé.<br>
+            Pour exercer vos droits ou pour toute question concernant vos données personnelles, veuillez nous contacter à [adresse e-mail de contact].<br>
+            <br>
+            Modifications
+            <br>
+            Nous nous réservons le droit de modifier cette politique à tout moment. Toute modification sera publiée sur cette page avec une mise à jour de la date de révision.
+            <br>
+            Nous vous remercions de votre confiance et vous souhaitons une agréable expérience sur Hackat'innov !
+
+        </p>
     </div>
 @endsection
