1b2b211db8bddad042615235a5a6c40d8f110a2c
Robin Tijou <robintijou05@gmail.com>
2024-11-02 14:46:33 +0100

    affichage des hackathons dans lapage archive

--- resources/views/main/archive.blade.php
+++ resources/views/main/archive.blade.php
@@ -7,47 +7,46 @@
 @endsection
 
 @section('content')
-    <div class="container mt-5">
+<div class="container mt-5">
 
     <a class="btn bg-green m-2 button-home" href="/archive">Tout afficher</a>
     <a class="btn bg-green m-2 button-home" href="/passedArchive">Hackathon passé</a>
     <a class="btn bg-green m-2 button-home" href="/incomingArchive">Hackathon futur</a>
     <?php
+    use App\Utils\SessionHelpers;
 
-use App\Utils\SessionHelpers;
+    // if (SessionHelpers::isConnected()) { ?>
+         <!--<a class="btn bg-green m-2 button-home" href="/archiveByEquipe?ide=<//?= $connected->nomequipe ?>">Hackathon de l'équipe</a><br>
+    // <//?php } ?>-->
 
- if (SessionHelpers::isConnected()) { ?>
-    <a class="btn bg-green m-2 button-home" href="/archiveByEquipe?ide=<?= $connected->nomequipe ?>">Hackathon de l'équipe</a><br>
-    <?php } ?>
-    <?php foreach ($hackathon as $h): ?>
-        <div class="row hackathon-item mb-5">
-            <div class="col-12 col-md-9 d-flex">
-                <img src="<?= $h->affiche ?>" class="affiche d-md-block d-none" alt="Affiche de l'évènement." style="min-width: 450px; margin-right: 20px;">
-                <div class="px-5">
-                    <h2><?= $h->thematique ?></h2>
-                    <p><?= nl2br($h->objectifs) ?></p>
-                    <p><?= nl2br($h->conditions) ?></p>
-
-                    <div class="card w-100 mt-3">
-                        <div class="card-body">
-                            <h5 class="card-title">Informations :</h5>
-                            <p class="card-text">
-                                <em>Date :</em> <?= date_create($h->dateheuredebuth)->format("d/m/Y H:i") ?>
-                                au <?= date_create($h->dateheurefinh)->format("d/m/Y H:i") ?><br>
-                                <em>Lieu :</em> <?= $h->ville ?>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    <?php endforeach; ?>
+    <table class="table table-striped mt-4">
+        <thead>
+            <tr>
+                <th>Thématique</th>
+                <th>Objectifs</th>
+                <th>Conditions</th>
+                <th>Date de début</th>
+                <th>Date de fin</th>
+                <th>Lieu</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($hackathon as $h): ?>
+                <tr>
+                    <td><?= $h->thematique ?></td>
+                    <td><?= nl2br($h->objectifs) ?></td>
+                    <td><?= nl2br($h->conditions) ?></td>
+                    <td><?= date_create($h->dateheuredebuth)->format("d/m/Y H:i") ?></td>
+                    <td><?= date_create($h->dateheurefinh)->format("d/m/Y H:i") ?></td>
+                    <td><?= $h->ville ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 
     <!-- Lien de pagination -->
     <div class="d-flex justify-content-center">
         {{ $hackathon->links('pagination::bootstrap-4') }}
     </div>
 </div>
-
-
 @endsection
